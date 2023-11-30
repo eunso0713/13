d37b3c9cf28bdd65bbf0cd91c4d209b726fc63e6
eunso0713 <aliennumber1one@sookmyung.ac.kr>
2023-11-30 15:29:47 +0900

    2

--- main.c
+++ main.c
@@ -4,27 +4,29 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 
-struct student{
-	int ID; // 학번 
-	char name[100]; // 이름 
-	double grade; // 학점 
+struct point{
+	int x;
+	int y;
 };
 	
 
 int main(void) {
+	//instance
+	struct point p1, p2;
+	int xdiff, ydiff;
+	double dist;
 	
-	//instance 선언 
-	struct student s1 = {231506, "Cho", 4.3};
-	//다른 값 변경 
-	s1.ID = 123456;
-	//strcpy(s1.name, “Eunso”);
-	//한 글자만 할거면
-	s1.name[0]='c';
-	s1.grade = 4.2;
+	printf("input p1 coordinate (x y): ");
+	scanf("%d %d", &p1.x, &p1.y);
 	
-	printf("ID : %d\n", s1.ID);
-	printf("name : %s\n", s1.name);
-	printf("grade : %f\n", s1.grade); 
+	printf("input p2 coordinate (x y): ");
+	scanf("%d %d", &p2.x, &p2.y);
+	
+	xdiff = p2.x - p1.x;
+	ydiff = p2.y - p1.y;
+	dist = sqrt (xdiff*xdiff + ydiff*ydiff);
+	
+	printf("distance: %f\n", dist);
 	system("PAUSE"); 
 	return 0;
 }
